a48642a1ab72485200419d8cb1dab99d587dabab
Salvador Guzman <guzmansalv@gmail.com>
2018-05-31 19:36:06 -0700

    fix

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ for book in books:
     detailsDict['desc'] = p
     detailsDict['categories'] = categories
 
-    print(title, file=sys.stderr)
+    sys.stderr.write(title)
     objects.append(detailsDict)
 
 print(json.dumps(objects, sort_keys=True, indent=4))
